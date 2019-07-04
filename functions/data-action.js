4fc22dfd24eb1dc64469da558bef37f2abc89fd6
Daniel <daniel.putschoegl@gmail.com>
2019-07-04 16:46:24 +0200

    Code commenting

--- functions/data-action.js
+++ functions/data-action.js
@@ -27,29 +27,33 @@ switch (type) {
         break;
 }
 
+// If anywhere data on 'weight' is published, this function is triggered
 eventHandler.subscribe('weight', function (data) {
+    // This function should measure the progress, so only if something is removed from the lorry, it is interesting.
     if (data.weightChange < 0) {
         models.Lorry.findByPk(data.lorryId).then(function (lorry) {
             if (!lorry) {
-
+                return;
             }
 
             models.Order.findByPk(lorry.OrderNr).then(function (order) {
                 if (!order || !order.lorryClosingEntry) {
-
+                    return;
                 }
 
+                // The Scan with the nearest weight is queried from the database
                 var sql = 'SELECT id, MIN(ABS(weight + ' + data.weightChange + ')) AS lowest FROM OrderScan WHERE on_lorry = 1 AND order_nr = \'' + order.nr + '\' GROUP BY id ORDER BY lowest LIMIT 1;';
                 sequelize.query(sql, {type: sequelize.QueryTypes.SELECT}).then(function (results) {
                     var orderScan = results[0];
                     if (!orderScan) {
-
+                        return;
                     }
 
+                    // The scan must match the tolerance
                     if (lower <= orderScan.lowest && orderScan.lowest <= upper) {
                         models.OrderScan.findByPk(orderScan.id).then(function (orderScan) {
                             if (!orderScan) {
-
+                                return;
                             }
 
                             orderScan.onLorry = false;
